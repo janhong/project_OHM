7c04da92071f9009c372002059d9f2634a38b689
janhong <janhong.jh@gmail.com>
2016-11-29 15:46:50 +0700

    Add files via upload

---
+++ ohm_jan_hong_levi.ino
@@ -0,0 +1,31 @@
+
+int vcab= 0;
+int Vin= 5;
+float Vout= 0;
+float R1= 100;
+float R2= 0;
+float adc= 0;
+
+void setup()
+{
+Serial.begin(9600);
+Serial.println("=================== Ω OHM Ω ==================");
+Serial.println("               Jan Hong & Levi ");
+}
+
+void loop()
+{
+vcab= analogRead(A0);
+if(vcab) 
+{
+vcab= analogRead(A0);
+adc= vcab * Vin;
+Vout= (adc)/1024.0;
+adc= (Vin/Vout) -1;
+R2= R1 * adc;
+Serial.print("\nVout: ");
+Serial.println(Vout);
+Serial.print("R2: ");
+Serial.println(R2);
+delay(5000);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    }
+}
